e9dae212eb9b9a7acf1a17ae9e9f8ba8d5080259
farhan121921 <122217319+farhan121921@users.noreply.github.com>
2024-09-02 23:50:04 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,4 +5,4 @@ apt-get update
 apt-get install -y libgl1-mesa-glx
 
 # Run your Streamlit app
-streamlit run app.py --server.port 8000 --server.address 0.0.0.0
+python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0
